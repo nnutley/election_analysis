264a84b95ad30f7068b6c16df97c41f0d66836be
nnutley <nnutley@arizona.edu>
2022-12-19 18:23:45 -0700

    get candidates in the election

--- PyPoll.py
+++ PyPoll.py
@@ -6,7 +6,6 @@
 # 5. The winner of the election based on popular vote
 
 # Add our dependencies
-
 import csv
 import os
 # Assign a variable for the file to load and the path.
@@ -14,15 +13,37 @@ file_to_load=os.path.join("Resources", "election_results.csv")
 #Assign a variable to save the file to a path
 file_to_save=os.path.join("analysis", "election_analysis.txt")
 
+
+# Initialize a total vote counter
+total_votes=0
+
+# Candidate Options
+candidate_options=[]
+
+
 #Open the election results and read the file
-
 with open(file_to_load) as election_data:
-
     #Read the file object with the reader function
     file_reader=csv.reader(election_data)
 
+
+    # Read the header row
+    headers=next(file_reader)
+
+
     #Print each row in the CSV file
-
     for row in file_reader:
+        # Add to the total vote count
+        total_votes +=1
+    
+        #Print the candidate name from each row
+        candidate_name=row[2]
 
-        print(row)
+        #If the candidate does not match any existing candidate...
+        if candidate_name not in candidate_options:
+
+            #Add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
+
+#3 Print the candidate list
+print(candidate_options)
