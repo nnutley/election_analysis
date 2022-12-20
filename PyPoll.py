0f6242dd8ed9e649a4c92d1f4d928b7ce95f41cf
nnutley <nnutley@arizona.edu>
2022-12-19 17:38:33 -0700

    added reader function

--- PyPoll.py
+++ PyPoll.py
@@ -1,32 +1,28 @@
-import csv
-import os
-# The data we need to retrieve/
+# The data we need to retrieve
 # 1. The total number of votes cast
 # 2. A complete list of candidates who recieved votes
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
 
-# Assign a variable for the file to load and the path.
+# Add our dependencies
 
+import csv
+import os
+# Assign a variable for the file to load and the path.
 file_to_load=os.path.join("Resources", "election_results.csv")
+#Assign a variable to save the file to a path
+file_to_save=os.path.join("analysis", "election_analysis.txt")
 
 #Open the election results and read the file
 
 with open(file_to_load) as election_data:
 
-    #To do: perform analysis.
+    #Read the file object with the reader function
+    file_reader=csv.reader(election_data)
 
-    print(election_data)
+    #Print each row in the CSV file
 
-# Create a filename variable to a direct or indirect path to the file
+    for row in file_reader:
 
-file_to_save=os.path.join("analysis", "election_analysis.txt")
-
-# Using the with statement open the file as a text file
-
-with open(file_to_save, "w") as txt_file:
-
-    # Write some data to the file
-
-    txt_file.write("Hello World")
+        print(row)
